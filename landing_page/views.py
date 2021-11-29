c380ae2d0ab60485e28ba6a208170232c1817523
ali ijaz <ijazali717@gmail.com>
2021-11-29 17:48:51 +0500

    front end changed

--- landing_page/views.py
+++ landing_page/views.py
@@ -1,7 +1,4 @@
 import os
-import subprocess
-from subprocess import call
-
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from depth.settings import BASE_DIR
@@ -10,10 +7,11 @@ from django.contrib import messages
 from datetime import datetime
 from PIL import Image
 
-# Create your views here.
+
 def home_page(request):
     if request.method == "POST":
         uploaded_file = request.FILES.get('file_input')
+
         current_date_time = datetime.now()
         year = str(current_date_time.strftime("%Y"))
         month = str(current_date_time.strftime("%m"))
@@ -22,14 +20,17 @@ def home_page(request):
 
         uploaded_file.name = year + "_" + month + "_" + day + "_" + time + ".png"
 
+
         file_object = File(name=uploaded_file)
         file_object.save()
-        file = os.path.join(BASE_DIR, 'media') + "/" + uploaded_file.name
-        resize_command = "mogrify -resize 50% " + file
+
+        image = os.path.join(BASE_DIR, 'media/') + uploaded_file.name
+        resize_command = "mogrify -resize 50% " + image
         os.system(resize_command)
         messages.success(request, 'File submitted and saved successful')
-        with open(file, 'rb') as fh:
-            response = HttpResponse(fh.read(), content_type='image/png')
+
+        with open(image, 'rb') as img:
+            response = HttpResponse(img.read(), content_type='image/png')
             response['Content-Disposition'] = 'attachment; filename=' + uploaded_file.name
         return response
     return render(request, 'landing_page/home.html')
